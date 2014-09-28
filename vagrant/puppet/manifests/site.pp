e7fa13b79461b3edd4c7217c6567e9e9a0574ec7
Jose Diaz <jose.diaz@arkonline.org>
2014-09-28 01:02:40 +0100

    Added Vagrant

---
+++ vagrant/puppet/manifests/site.pp
@@ -0,0 +1,39 @@
+# To re-run this file:
+# sudo puppet apply /vagrant/puppet/manifests/site.pp --modulepath="/vagrant/puppet/modules/" --hiera_config /vagrant/hiera.yaml
+
+Exec {
+  path => [
+    '/bin/',
+    '/sbin/',
+    '/usr/bin/',
+    '/usr/sbin/',
+    '/usr/local/bin/',
+  ]
+}
+
+package {
+  [
+    'build-essential',
+    'ruby-dev',
+#    'npm',
+    'nodejs-legacy',
+  ]:
+    ensure => 'latest'
+}
+
+exec { 'gem install bundler':
+  command => 'sudo gem install bundler',
+  creates => '/var/lib/gems/1.9.1/gems/bundler-1.7.3',
+  require => [Package['ruby-dev'], Package['build-essential']]
+}
+
+exec { 'bundle install':
+  command => 'bundle install',
+  creates => '/var/lib/gems/1.9.1/gems/jekyll-2.4.0',
+  cwd     => '/var/www/anguloceramicart',
+  require => Exec['gem install bundler'],
+}
+
+exec { 'sudo npm install -g bower':
+  require => Exec['nodejs-legacy'],
+}
